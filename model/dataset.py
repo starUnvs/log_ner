1ee12f8280c62577f591cb2109a4f7027113c155
starUnvs <23546860+starUnvs@users.noreply.github.com>
2021-07-16 18:06:58 +0800

    limit the max length of input seq

--- model/dataset.py
+++ model/dataset.py
@@ -17,17 +17,18 @@ class LogDataset(torch.utils.data.Dataset):
         return self.x_ids[idx], self.y_ids[idx]
 
 
-def collate_fn(batch, return_tensor=True, x_pad_idx=0, y_pad_idx=0):
-    b_input_ids, b_tag_ids = [t[0] for t in batch], [t[1] for t in batch]
+def collate_fn(batch, return_tensor=True, x_pad_idx=0, y_pad_idx=0, max_len=200):
+    b_input_ids, b_tag_ids = [t[0][:max_len]
+                              for t in batch], [t[1][:max_len] for t in batch]
 
     seq_lens = [len(inputs) for inputs in b_input_ids]
     b_masks = len2mask(seq_lens)
 
     # pad seq in bath to same length
-    max_len = max(seq_lens)
-    b_input_ids = [pad(input_ids, x_pad_idx, max_len)
+    b_max_len = max(seq_lens)
+    b_input_ids = [pad(input_ids, x_pad_idx, b_max_len)
                    for input_ids in b_input_ids]
-    b_tag_ids = [pad(input_labels, y_pad_idx, max_len)
+    b_tag_ids = [pad(input_labels, y_pad_idx, b_max_len)
                  for input_labels in b_tag_ids]
 
     if return_tensor:
--- train.py
+++ train.py
@@ -6,29 +6,30 @@ from seqeval.metrics import classification_report, f1_score
 from seqeval.scheme import IOB2
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, RandomSampler
-from tqdm import tqdm, trange
+from tqdm import tqdm
 
 from model.bilstm_crf import BiLSTMCRF
 from model.dataset import LogDataset, collate_fn
 
-DATA_FILE_PATH = './inputs/apache.csv'
+DATA_FILE_PATH = './inputs/train.csv'
 VOCAB_PATH = './vocab/vocab_full.json'
 TAG_VOCAB_PATH = './vocab/tag_vocab.json'
-MODEL_SAVE_FILE_PATH = './trained_model/model.pth'
+MODEL_SAVE_FILE_PATH = './trained_model/model_full.pth'
 OPT_FILE_PATH = './trained_model/opt.pth'
 
 epochs = 10
 batch_size = 32
 max_grad_norm = 5
-lr = 0.001
-lr_decay = 0.5
-max_patience, max_decay = 2, 2
+lr = 0.0005
+max_patience = 2
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 tag2idx: dict
 with open(TAG_VOCAB_PATH, 'r') as f:
     tag2idx = json.load(f)
+    tag2idx['B-<START>'] = tag2idx.pop('<START>')
+    tag2idx['B-<END>'] = tag2idx.pop('<END>')
 idx2tag = {i: tag for tag, i in tag2idx.items()}
 vocab: dict
 with open(VOCAB_PATH, 'r') as f:
@@ -38,7 +39,7 @@ with open(VOCAB_PATH, 'r') as f:
 def train(model, optimizer, train_dataloader):
     model.train()
 
-    report_every = 100
+    report_every = 500
     tr_loss, n_sentences = 0, 0
 
     for step, batch in enumerate(tqdm(train_dataloader)):
@@ -106,12 +107,15 @@ def val(model, val_dataloader):
             ['O' if tag[0] == 'X' else tag for tag in tags] for tags in tmp_true_tags]
 
         # exclude <START> and <END>
-        pred_tags.extend([tags[1:-1] for tags in tmp_pred_tags])
-        true_tags.extend([tags[1:-1] for tags in tmp_true_tags])
+        pred_tags.extend([tags for tags in tmp_pred_tags])
+        true_tags.extend([tags for tags in tmp_true_tags])
 
     print(f'Avg Validation loss: {val_loss/n_sentences}')
-    print(f'Validation F1: {f1_score(true_tags,pred_tags,scheme=IOB2)}')
-    print(classification_report(true_tags, pred_tags, scheme=IOB2))
+    print(f'Validation F1: {f1_score(true_tags,pred_tags)}')
+    print(classification_report(true_tags, pred_tags))
+    print(
+        f"Validation F1 STRICT: {f1_score(true_tags,pred_tags,mode='strict',scheme=IOB2)}")
+    print(classification_report(true_tags, pred_tags, scheme=IOB2, mode='strict'))
 
     return val_loss/n_sentences
 
@@ -137,10 +141,12 @@ if __name__ == '__main__':
     model = BiLSTMCRF(len(vocab), len(tag2idx)).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, factor=0.5, verbose=True)
+        optimizer, factor=0.1, verbose=True)
 
     best_val_loss = float('inf')
-    for _ in trange(epochs, desc="Epoch"):
+    for epoch in range(epochs):
+        print(f'EPOCH: {epoch}/{epochs}')
+
         train(model, optimizer, train_dataloader)
         val_loss = val(model, val_dataloader)
         scheduler.step(val_loss)
